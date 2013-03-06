ce5707e87ea940c183e56f58a485ce166ff60771
charlie <java@elvenware.com>
2013-03-06 13:34:49 -0800

    Adding in first drafts for weekly tips for Weeks 1 - 9

--- Prog272/Convert.bat
+++ Prog272/Convert.bat
@@ -1,2 +1,10 @@
-markdowntocloudnotes Week09.md Prog272\Week09.html
-markdowntocloudnotes Week10.md Prog272\Week10.html 
+call markdowntocloudnotes Week01.md Prog272\Week01.html
+call markdowntocloudnotes Week02.md Prog272\Week02.html
+call markdowntocloudnotes Week03.md Prog272\Week03.html
+call markdowntocloudnotes Week04.md Prog272\Week04.html
+call markdowntocloudnotes Week05.md Prog272\Week05.html
+call markdowntocloudnotes Week06.md Prog272\Week06.html
+call markdowntocloudnotes Week07.md Prog272\Week07.html
+call markdowntocloudnotes Week08.md Prog272\Week08.html
+call markdowntocloudnotes Week09.md Prog272\Week09.html
+call markdowntocloudnotes Week10.md Prog272\Week10.html 
