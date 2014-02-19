93f6612c1e8278059b8efbbec7223de1d8a396e2
Charlie CedarIsle Calvert <charlie@elvenware.com>
2014-02-19 10:37:15 -0800

    'asdf'

--- Prog272/PandocBuild.bat
+++ Prog272/PandocBuild.bat
@@ -1,1 +1,1 @@
-pandoc --section-divs -t html5 -s --template template.revealjs -o Week06.html Week06a.md 
+pandoc --section-divs -t html5 -s --template template.revealjs01 -o Week06.html Week06a.md 
