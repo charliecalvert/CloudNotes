e7dda1632a8e5165aa89cd151faa0f3ac3d6bebc
Charlie on Linux Ubuntu Main <charlie@elvenware.com>
2018-10-11 10:57:04 -0700

    new code: 10-11-2018

--- remote.js
+++ remote.js
@@ -1,48 +1,97 @@
 const exec = require('child_process').exec;
 const spawn = require('child_process').spawn;
-    
-const execPush = () => {
-   
-    var yourscript = exec('~/Git/CloudNotes/push "new code"',
-        (error, stdout, stderr) => {            
-            // console.log(`${stderr}`);
-            if (error !== null) {
-                console.log(`exec error: ${error}`);
-            }
-        });
-        
-	yourscript.stdout.on('data', (data) => {
-		console.log('PUSH', data);
-	});
-	
-	yourscript.stderr.on('data', (data) => {
-		console.error('PUSH', data);
-	});
-	
-	yourscript.on('close', (code) => {
-		console.log(`PUSH exited with code ${code}`);
-	});
 
-}
+const execPush = () => {
+
+    return new Promise(function (resolve, reject) {
+
+        var yourscript = exec('~/Git/CloudNotes/push "new code"',
+            (error, stdout, stderr) => {
+                // console.log(`${stderr}`);
+                if (error !== null) {
+                    console.log(`exec error: ${error}`);
+                }
+            });
+
+        yourscript.stdout.on('data', (data) => {
+            console.log('PUSH', data);
+        });
+
+        yourscript.stderr.on('data', (data) => {
+            console.error('PUSH', data);
+        });
+
+        yourscript.on('close', (code) => {
+            resolve({
+                result: 'success',
+                code: code
+            });
+        });
+
+        yourscript.on('error', (code) => {
+            reject({
+                result: 'error',
+                code: code
+            });
+        });
+    });
+};
 
 const execSend = () => {
 
-    
+    return new Promise(function (resolve, reject) {
+        var yourscript = spawn('/home/charlie/Git/CloudNotes/send', ['test args']);
+        //var yourscript = spawn('/home/charlie/Git/CloudNotes/send test args');
+
+        yourscript.stdout.on('data', (data) => {
+            console.log(`child stdout:\n${data}`);
+            //console.log("SEND", data);
+        });
+
+        yourscript.stderr.on('data', (data) => {
+            console.log(`child stderr:\n${data}`);
+            //console.error("SEND", data);
+        });
+
+        yourscript.on('close', (code) => {
+            //console.log(`send exited with code ${code}`);
+            resolve({
+                result: 'success',
+                code: code
+            });
+        });
+
+        yourscript.on('error', (code) => {
+            reject(code);
+            // console.log(`send errored with code ${code}`);		
+        });
+
+    });
+};
+
+
+const execSend2 = () => {
+
     var yourscript = spawn('/home/charlie/Git/CloudNotes/send', ['test args']);
-        
-	yourscript.stdout.on('data', (data) => {
-		console.log(`child stdout:\n${data}`);
-		//console.log("SEND", data);
-	});
-	
-	yourscript.stderr.on('data', (data) => {
-		console.log(`child stderr:\n${data}`);
-		//console.error("SEND", data);
-	});
-	
-	yourscript.on('close', (code) => {
-		console.log(`send exited with code ${code}`);
-	});
+    //var yourscript = spawn('/home/charlie/Git/CloudNotes/send test args');
+
+    yourscript.stdout.on('data', (data) => {
+        console.log(`child stdout:\n${data}`);
+        //console.log("SEND", data);
+    });
+
+    yourscript.stderr.on('data', (data) => {
+        console.log(`child stderr:\n${data}`);
+        //console.error("SEND", data);
+    });
+
+    yourscript.on('close', (code) => {
+        console.log(`send exited with code ${code}`);
+    });
+
+    yourscript.on('error', (code) => {
+        console.log(`send errored with code ${code}`);
+    });
 
 }
 
@@ -50,16 +99,16 @@ const ssher = () => {
     var Client = require('ssh2').Client;
 
     var conn = new Client();
-    conn.on('ready', function() {
+    conn.on('ready', function () {
         console.log('Client :: ready');
-        conn.exec('~/bin/pull-key-repos', function(err, stream) {
+        conn.exec('~/bin/pull-key-repos', function (err, stream) {
             if (err) throw err;
-            stream.on('close', function(code, signal) {
+            stream.on('close', function (code, signal) {
                 console.log('Stream :: close :: code: ' + code + ', signal: ' + signal);
                 conn.end();
-            }).on('data', function(data) {
+            }).on('data', function (data) {
                 console.log('STDOUT: ' + data);
-            }).stderr.on('data', function(data) {
+            }).stderr.on('data', function (data) {
                 console.log('STDERR: ' + data);
             });
         });
@@ -70,8 +119,23 @@ const ssher = () => {
         privateKey: require('fs').readFileSync(process.env.HOME + '/.ssh/Home-2016-03')
     });
 
-}
+};
 
-execPush();
-execSend();
-ssher();
+execPush()
+    .then((code) => {
+        console.log(`send exited with result: ${code.result} and code: ${code.code}`);
+    })
+    .catch((code) => {
+        console.log(`send errored with result: ${code.result} and code: ${code.code}`);
+    });
+
+execSend()
+    .then((code) => {
+        console.log(`send exited with resule: ${code.result} and code: ${code.code}`);
+    })
+    .catch((code) => {
+        console.log(`send errored with code ${code}`);
+    });
+
+
+//ssher();
