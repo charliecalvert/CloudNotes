c138899c7a6790a4c8bf4ab9bb871ec0d36b05aa
Charlie on Linux Ubuntu Main <charlie@elvenware.com>
2018-10-11 09:31:40 -0700

    new code: 10-11-2018

--- remote.js
+++ remote.js
@@ -1,5 +1,6 @@
-const exec = require('child_process').exec;
-var yourscript = exec('./push "input test"',
+const execer = () => {
+    const exec = require('child_process').exec;
+    var yourscript = exec('./push "new code"',
         (error, stdout, stderr) => {
             console.log(`${stdout}`);
             console.log(`${stderr}`);
@@ -7,3 +8,33 @@ var yourscript = exec('./push "input test"',
                 console.log(`exec error: ${error}`);
             }
         });
+}
+
+const ssher = () => {
+    var Client = require('ssh2').Client;
+
+    var conn = new Client();
+    conn.on('ready', function() {
+        console.log('Client :: ready');
+        conn.exec('~/bin/pull-key-repos', function(err, stream) {
+            if (err) throw err;
+            stream.on('close', function(code, signal) {
+                console.log('Stream :: close :: code: ' + code + ', signal: ' + signal);
+                conn.end();
+            }).on('data', function(data) {
+                console.log('STDOUT: ' + data);
+            }).stderr.on('data', function(data) {
+                console.log('STDERR: ' + data);
+            });
+        });
+    }).connect({
+        host: '52.35.150.195',
+        port: 22,
+        username: 'ubuntu',
+        privateKey: require('fs').readFileSync(process.env.HOME + '/.ssh/Home-2016-03')
+    });
+
+}
+
+execer();
+//ssher();
