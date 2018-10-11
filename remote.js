9ed6b325b41c8dd523de454d52729299eb38a3ae
Charlie on Linux Ubuntu Main <charlie@elvenware.com>
2018-10-11 08:57:51 -0700

    test: 10-11-2018

---
+++ remote.js
@@ -0,0 +1,9 @@
+const exec = require('child_process').exec;
+var yourscript = exec('./deploy "test"',
+        (error, stdout, stderr) => {
+            console.log(`${stdout}`);
+            console.log(`${stderr}`);
+            if (error !== null) {
+                console.log(`exec error: ${error}`);
+            }
+        });
