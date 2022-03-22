774f71259b40d40fb64443d8c278854367c62e4d
charlie on WSL HP-Desktop <charlie@elvenware.com>
2022-03-21 18:29:18 -0700

    recreate all ids stll broken

---
+++ src/RecreateAllIds.js
@@ -0,0 +1,44 @@
+const fsp = require('fs').promises;
+const elfUtils = require('elven-code').elfUtils;
+
+const walker = require('walk-directories').walker;
+const debugMain = require('debug')('check-main');
+const debugDetail = require('debug')('check-main-detail');
+const { setupElfCode } = require('../lib/getElfCode');
+
+
+// Then, use it with a simple async for loop
+// https://stackoverflow.com/a/36856787/253576
+async function main() {
+    let count = 0;
+    const matterData = [];
+    for await (const relativePath of walker('../.')) {
+        count++;
+        const fileName = elfUtils.ensureEndsWithPathSep(__dirname) + relativePath;
+        debugMain('fileName', fileName);
+        debugMain('getTitle', elfUtils.getTitleFromPath(fileName));
+        if (fileName.includes('About.md')) {
+            debugDetail(fileName);
+        }
+        const elfCodes = await setupElfCode(fileName, relativePath);
+    
+        
+        if (elfCodes.data) {
+            elfCodes.data.id = count;
+            matterData.push(elfUtils.objectToJson(elfCodes.data));
+            // await fsp.writeFile(fileName, elfCodes.markdown, 'utf8');
+            debugMain('count', count);
+        }
+    }
+    // if (count === 100) {
+    // const fsp = require('fs').promises;
+    const json = JSON.stringify(matterData, null, 4);
+    // await fsp.writeFile('all-matter.json', json, 'utf8');
+    // return;
+    // }
+}
+
+exports.main = main;
+main().catch(console.error);
+
+
