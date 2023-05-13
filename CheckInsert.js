de7bbd8373969327c8292b91f538902e3ad97f09
charlie on CascadeTrails <charlie@elvenwaree.com>
2023-05-13 12:26:32 -0700

    linting

--- CheckInsert.js
+++ CheckInsert.js
@@ -1,28 +1,24 @@
-//const toc = require('markdown-toc');
-
 const insert = require('./lib/insert.js');
 
 const elfUtils = require('elven-code').elfUtils;
 const fileName = './CloudNotes.md';
-const elf = require('markdown-elf');
+// const elf = require('markdown-elf');
 const toc = require('markdown-toc');
 
 async function getFile(str, options) {
-    var regex = options.regex || /(?:<!-- toc(?:\s*stop)? -->)/g;
-    var open = typeof options.open === 'string' ? options.open : '<!-- toc -->\n\n';
-    var close = typeof options.close === 'string' ? options.close : '<!-- tocstop -->';
+    // const regex = options.regex || /(?:<!-- toc(?:\s*stop)? -->)/g;
+    const open = typeof options.open === 'string' ? options.open : '<!-- toc -->\n\n';
+    const close = typeof options.close === 'string' ? options.close : '<!-- tocstop -->';
 
-    let markdown = await elfUtils.readFileAsync(fileName);
+    const markdown = await elfUtils.readFileAsync(fileName);
     console.log(open);
     console.log(close);
     console.log(insert(markdown));
 }
 
 async function getMarkdown(str, options) {
-   
-
     let markdown = await elfUtils.readFileAsync(fileName);
-    markdown = toc.insert(markdown)
+    markdown = toc.insert(markdown);
     console.log(insert(markdown));
 }
 /* getFile('foobar', {
@@ -32,3 +28,8 @@ async function getMarkdown(str, options) {
 }); */
 
 getMarkdown('foobar');
+getFile('foobar', {
+    open: '<!-- bar -->',
+    close: '<!-- barstop -->',
+    regex: /(?:<!-- bar(?:\s*stop)? -->)/g
+});
